1f28cf9aebc48acebfbd2753a09cf6f94d326e66
AndreasAbdi <andreasabdi@yahoo.ca>
2016-11-06 21:07:38 -0800

    [GAME] Added basic game logic for snake and food.
    A snake is now in the screen. It eats food. If food is eaten, the snake grows. 
    Snake cannot die. 
    Snake can fly into the edges of heaven and ignore everything.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,222 @@
 #pragma once
 #include "SDL.h"
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+
+static int SCREEN_HEIGHT = 640;
+static int SCREEN_WIDTH = 640;
+static int BLOCK_SIZE = 10;
 
 using namespace std;
 
+struct Color {
+	Uint8 r,g,b,a;
+};
+
+struct SnakeSection {
+	int x, y;
+	SnakeSection(int inX, int inY) : x(inX), y(inY) {};
+};
+
+struct Snake {
+private:
+
+	void moveHeadTowardsCurrentDirection() {
+		switch (currentDirection) {
+		case 'l':
+			snakeSections.front().x -= BLOCK_SIZE;
+			break;
+		case 'r':
+			snakeSections.front().x += BLOCK_SIZE;
+			break;
+		case 'u':
+			snakeSections.front().y -= BLOCK_SIZE;
+			break;
+		case 'd':
+			snakeSections.front().y += BLOCK_SIZE;
+			break;
+		default:
+			break;
+		}
+	}
+	void moveTailSectionsTowardsHead() {
+		for (auto snakeSection = snakeSections.rbegin(); (snakeSection + 1) != snakeSections.rend(); ++snakeSection) {
+			SnakeSection sectionAheadOfCurrent = *(snakeSection + 1);
+			snakeSection->x = sectionAheadOfCurrent.x;
+			snakeSection->y = sectionAheadOfCurrent.y;
+		}
+	}
+public:
+	int numberSections;
+	char currentDirection;
+	vector<SnakeSection> snakeSections;
+public:
+	Snake(int initialX, int initialY, char direction) : currentDirection(direction), numberSections(1) {
+		snakeSections.push_back(SnakeSection(initialX, initialY));
+	};
+
+	//TODO: Make it so you can't collide into yourself. so you cannot invert your direction. 
+	void setDirection(char direction) {
+		this->currentDirection = direction;
+	};
+
+	
+	void move() {
+		moveTailSectionsTowardsHead();
+		moveHeadTowardsCurrentDirection();
+	};
+
+	void addSection() {
+		numberSections++;
+		SnakeSection endSection = snakeSections.back();
+		snakeSections.push_back(endSection);
+	};
+};
+
+struct Food {
+	int x, y;
+	Food() : Food(100, 100) {};
+	Food(int inX, int inY) : x(inX), y(inY) {};
+	void setPosition(int inX, int inY) {
+		this->x = inX;
+		this->y = inY;
+	};
+};
+
+class Game {
+	SDL_Window * window;
+	SDL_Renderer * renderer;
+	SDL_Texture * texture;
+
+	int score = 0;
+	Snake snake;
+	Food food;
+private:
+	void drawRectangle(int x, int y, Color color, int width = 10, int height = 10) {
+		SDL_Rect rectangle = { x,y,width,height };
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+		SDL_RenderFillRect(renderer, &rectangle);
+	};
+
+	void drawBorders() {
+		Color borderColor = { 0xFF, 0x00, 0x00, 0xFF };
+		drawRectangle(0, 0, borderColor, 640, 10);
+		drawRectangle(0, 630, borderColor, 640,10);
+		drawRectangle(0, 0, borderColor, 10, 640);
+		drawRectangle(630, 0, borderColor, 10, 640);
+	};
+
+	void drawFood() {
+		Color borderColor = { 0xFF, 0xFF, 0x00, 0xFF };
+
+		drawRectangle(food.x, food.y, borderColor);
+	};
+
+	void drawSnake() {
+		for (auto snakePart = snake.snakeSections.begin(); snakePart != snake.snakeSections.end(); ++snakePart) {
+			Color borderColor = { 0x00, 0xFF, 0x00, 0xFF };
+
+			drawRectangle(snakePart->x, snakePart->y, borderColor);
+		}
+	};
+
+	bool snakeCollidesWithFood() {
+		SnakeSection snakeHead = snake.snakeSections.front();
+		bool sameX = (food.x == snakeHead.x);
+		bool sameY = (food.y == snakeHead.y);
+		return  sameX && sameY;
+	};
+
+	void addFood() {
+		int newX = rand() % (SCREEN_WIDTH - 20) + 10;
+		newX = newX - (newX % 10);
+		int newY = rand() % (SCREEN_HEIGHT - 20) + 10;
+		newY = newY - (newY % 10);
+		food.setPosition(newX, newY );
+	};
+public:
+	Game() : snake(400,400,'l') {
+		window = SDL_CreateWindow("SNEK",
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+	};
+	
+	void handleKeyPress(const SDL_Event event) {
+		switch (event.key.keysym.sym) {
+		case SDLK_a:
+			snake.setDirection('l');
+			break;
+		case SDLK_s:
+			snake.setDirection('d');
+			break;
+		case SDLK_d:
+			snake.setDirection('r');
+			break;
+		case SDLK_w:
+			snake.setDirection('u');
+			break;
+		}
+	};
+
+	void drawGame() {
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		drawBorders();
+		drawSnake();
+		drawFood();
+		SDL_RenderPresent(renderer);
+	};
+
+	void start() {
+		srand(0);
+		food.setPosition(300, 300);
+	};
+
+	void runState() {
+		if (snakeCollidesWithFood()) {
+			score++;
+			snake.addSection();
+			addFood();
+		}
+		snake.move();	
+		drawGame();
+	};
+
+	//TODO: add collision so that you die when you hit walls or yourself. 
+	//TODO: add reset of game state.
+
+	void terminate() {
+		SDL_DestroyTexture(texture);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+	};
+
+
+};
+
 int main(int argc, char* args[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window * window = SDL_CreateWindow("SNEK",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, SDL_WINDOW_SHOWN);
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_STATIC, 640, 640);
+	Game game;
+	game.start();
 
-	SDL_Surface * image = SDL_LoadBMP("assets/lena_gray.bmp");
-	SDL_Texture * imageTexture = SDL_CreateTextureFromSurface(renderer, image);
-	SDL_FreeSurface(image);
-
-	for (int i = 0; i < 2; ++i) {
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, imageTexture, NULL, NULL);
-		SDL_RenderPresent(renderer);
-		SDL_Delay(1000);
+	bool quit = false;
+	SDL_Event event;
+	while (!quit) {
+		while (SDL_PollEvent(&event) != 0) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			} else if (event.type == SDL_KEYDOWN) {
+				game.handleKeyPress(event);
+			}
+		}
+		game.runState();
+		SDL_Delay(250);
 	}
 
-	SDL_DestroyTexture(imageTexture);
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+	game.terminate();
 	SDL_Quit();
 
 	return 0;
